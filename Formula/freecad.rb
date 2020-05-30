509d22243e2e5760c68383d35eb33a2bfcb970f8
Yohey Ishizu <yohey.ishizu@gmail.com>
2020-05-30 20:11:08 +0900

    freecad: version 0.19_pre, change dependencies, apply patch

--- Formula/freecad.rb
+++ Formula/freecad.rb
@@ -1,9 +1,10 @@
 class Freecad < Formula
   desc "Parametric 3D modeler"
   homepage "http://www.freecadweb.org"
-  url "https://github.com/FreeCAD/FreeCAD/archive/0.17.tar.gz"
-  version "0.17"
-  sha256 "ae017393476b6dc7f1192bcaf91ceedc2f9b791f2495307ce7c45efadb5266fb"
+  url "https://github.com/FreeCAD/FreeCAD/archive/0.19_pre.tar.gz"
+  # url "https://github.com/FreeCAD/FreeCAD.git", :using => :git, :branch => "0.19_pre"
+  version "0.19_pre"
+  sha256 "b16c17e83496ec34cd29ed208f4d6049984879ef953574dc3729a4be06e35b32"
   head "https://github.com/FreeCAD/FreeCAD.git", :branch => "master"
 
   # Debugging Support
@@ -22,32 +23,34 @@ class Freecad < Formula
   # Required dependencies
   depends_on :macos => :mavericks
   depends_on "freetype"
-  depends_on "python@2"
-  depends_on "boost-python"
+  depends_on "python@3.8"
+  depends_on "boost-python3"
   depends_on "xerces-c"
   if build.with?("qt4")
     depends_on "cartr/qt4/qt@4"
     depends_on "cartr/qt4/pyside-tools@1.2"
   else
-    depends_on "qt"
-    depends_on "qtwebkit"
-    depends_on "FreeCAD/freecad/pyside2-tools"
+    depends_on "yohey/legacy/qt_5.11"
+    depends_on "yohey/legacy/qtwebkit"
+    depends_on "yohey/legacy/pyside2-tools"
     depends_on "webp"
   end
-  depends_on "opencascade"
+  depends_on "yohey/opencae/opencascade@7.3"
   depends_on "orocos-kdl"
-  depends_on "freecad/freecad/matplotlib"
-  depends_on "freecad/freecad/med-file"
+  depends_on "yohey/legacy/matplotlib@2.1"
+  depends_on "yohey/opencae/med-file"
   depends_on "vtk"
-  depends_on "FreeCAD/freecad/nglib"
+  depends_on "yohey/opencae/nglib@5.3"
   depends_on "FreeCAD/freecad/coin"
-  depends_on "FreeCAD/freecad/pivy"
-  depends_on "swig" => :build
+  depends_on "yohey/opencae/pivy@0.6"
+  depends_on "swig@3" => :build
 
   if build.with?("packaging-utils")
     depends_on "node"
     depends_on "jq"
   end
+
+  patch :DATA
 
   def install
     if build.with?("packaging-utils")
@@ -58,13 +61,20 @@ class Freecad < Formula
     args = std_cmake_args
     if build.without?("qt4")
       args << "-DBUILD_QT5=ON"
-      args << '-DCMAKE_PREFIX_PATH="' + Formula["qt"].opt_prefix + "/lib/cmake;" + Formula["qtwebkit"].opt_prefix + '/lib/cmake"'
+      args << '-DCMAKE_PREFIX_PATH="' + Formula["qt_5.11"].opt_prefix + "/lib/cmake;" + Formula["yohey/legacy/qtwebkit"].opt_prefix + '/lib/cmake"'
     end
-    args << %W[
-      -DBUILD_FEM_NETGEN:BOOL=ON
-      -DFREECAD_USE_EXTERNAL_KDL=ON
-      -DCMAKE_BUILD_TYPE=#{build.with?("debug") ? "Debug" : "Release"}
-    ]
+    args << "-DBUILD_FEM_NETGEN:BOOL=ON"
+    args << "-DFREECAD_USE_EXTERNAL_KDL=ON"
+    args << "-DCMAKE_BUILD_TYPE=#{build.with?("debug") ? "Debug" : "Release"}"
+    args << "-DFREECAD_CREATE_MAC_APP=ON"
+
+    python = Formula["python@3.8"]
+    pyhome = `#{python.opt_bin}/python3-config --prefix`.chomp
+    pyver = Language::Python.major_minor_version "#{pyhome}/bin/python3"
+
+    args << "-DPYTHON_EXECUTABLE=#{pyhome}/bin/python#{pyver}"
+    args << "-DPYTHON_LIBRARY=#{pyhome}/lib/libpython#{pyver}.dylib"
+    args << "-DPYTHON_INCLUDE_DIR=#{pyhome}/include/python#{pyver}"
 
     mkdir "Build" do
       system "cmake", *args, ".."
@@ -72,7 +82,7 @@ class Freecad < Formula
     end
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; <<~EOS
     After installing FreeCAD you may want to do the following:
 
     1. Amend your PYTHONPATH environmental variable to point to
@@ -81,3 +91,27 @@ class Freecad < Formula
     EOS
   end
 end
+
+__END__
+diff --git a/src/MacAppBundle/CMakeLists.txt b/src/MacAppBundle/CMakeLists.txt
+index 91a56866a..b6c25ce55 100644
+--- a/src/MacAppBundle/CMakeLists.txt
++++ b/src/MacAppBundle/CMakeLists.txt
+@@ -31,6 +31,7 @@ if(HOMEBREW_PREFIX)
+     foreach(PTH_FILE ${HOMEBREW_PTH_FILES})
+         file(READ ${PTH_FILE} ADDITIONAL_DIR)
+ 
++        if(EXISTS "${ADDITIONAL_DIR}")
+	string(STRIP "${ADDITIONAL_DIR}" ADDITIONAL_DIR)
+         string(REGEX REPLACE "^${HOMEBREW_PREFIX}/Cellar/([A-Za-z0-9_]+).*$" "\\1" LIB_NAME ${ADDITIONAL_DIR})
+         string(REGEX REPLACE ".*libexec(.*)/site-packages" "libexec/${LIB_NAME}\\1" NEW_SITE_DIR ${ADDITIONAL_DIR})
+@@ -45,6 +46,9 @@ if(HOMEBREW_PREFIX)
+		 \"../../../${NEW_SITE_DIR}/site-packages\"
+             )"
+         )
++        else()
++          message("Warning: The file \"${PTH_FILE}\" contains wrong path.")
++        endif()
+     endforeach(PTH_FILE)
+ endif()
+ 
