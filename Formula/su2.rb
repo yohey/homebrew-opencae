d434f043c2850799f9f7a0f91bd386066ca36fa0
Yohey Ishizu <yohey.ishizu@gmail.com>
2020-05-31 09:19:01 +0900

    su2: v7.0.5

---
+++ Formula/su2.rb
@@ -0,0 +1,38 @@
+class Su2 < Formula
+  desc "SU2: An Open-Source Suite for Multiphysics Simulation and Design"
+  homepage "https://su2code.github.io"
+  url "https://github.com/su2code/SU2/archive/v7.0.5.tar.gz"
+  version "v7.0.5"
+  sha256 "3cb2b87ef6ad3d31011756ca1da068fc8172c0d2d1be902fbbd4800b50da28bd"
+  head "https://github.com/su2code/SU2.git", :branch => "master"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+
+  depends_on "pkg-config" => :build
+  depends_on "re2c" => :build
+  depends_on "swig" => :build
+
+  depends_on "openblas" => :optional
+  depends_on "open-mpi" => :optional
+  depends_on "python@3.8" => :optional
+
+  depends_on "mpi4py" if build.with?("python@3.8") && build.with?("open-mpi")
+
+  def install
+    args = std_meson_args
+
+    args << "-Denable-openblas=true" if build.with?("openblas")
+    args << "-Denable-pywrapper=true" if build.with?("python@3.8")
+
+    mkdir "build" do
+      system "meson", *args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
+  end
+
+  test do
+    system "SU2_CFD", "--help"
+  end
+end
