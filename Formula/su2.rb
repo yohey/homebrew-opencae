ed24b037c8bb49a3c79af63d943c177967b439db
Yohey Ishizu <yohey.ishizu@gmail.com>
2021-09-17 20:25:09 +0900

    su2: v7.2.0

--- Formula/su2.rb
+++ Formula/su2.rb
@@ -1,9 +1,9 @@
 class Su2 < Formula
   desc "SU2: An Open-Source Suite for Multiphysics Simulation and Design"
   homepage "https://su2code.github.io"
-  url "https://github.com/su2code/SU2/archive/v7.1.1.tar.gz"
-  version "v7.1.1"
-  sha256 "6ed3d791209317d5916fd8bae54c288f02d6fe765062a4e3c73a1e1c7ea43542"
+  url "https://github.com/su2code/SU2/archive/v7.2.0.tar.gz"
+  version "v7.2.0"
+  sha256 "e929f25dcafc93684df2fe0827e456118d24b8b12b0fb74444bffa9b3d0baca8"
   head "https://github.com/su2code/SU2.git", :branch => "master"
 
   option "with-debug", "Enable debug build"
@@ -20,8 +20,6 @@ class Su2 < Formula
   depends_on "python" => :optional
 
   depends_on "mpi4py" if build.with?("python") && build.with?("open-mpi")
-
-  patch :DATA
 
   def install
     args = std_meson_args
@@ -46,13 +44,3 @@ class Su2 < Formula
 end
 
 __END__
---- a/Common/src/geometry/CPhysicalGeometry.cpp
-+++ b/Common/src/geometry/CPhysicalGeometry.cpp
-@@ -5929,6 +5929,7 @@ void CPhysicalGeometry::SetTurboVertex(CConfig *config, unsigned short val_iZone
-     /*--- to be set for all the processor to initialize an appropriate number of frequency for the NR BC ---*/
-     if(nVert > nVertMax){
-       SetnVertexSpanMax(marker_flag,nVert);
-+      nVertMax = nVert;
-     }
-     /*--- for all the processor should be known the amount of total turbovertex per span  ---*/
-     nTotVertex_gb[iSpan]= (int)nVert;
