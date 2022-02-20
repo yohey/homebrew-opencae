2a3e881cb12d25620f10df72bcbc0fca9342c8cf
Yohey Ishizu <yohey.ishizu@gmail.com>
2022-02-20 18:38:41 +0900

    endftk: add patch for arm

--- Formula/endftk.rb
+++ Formula/endftk.rb
@@ -3,6 +3,7 @@ class Endftk < Formula
   homepage "https://github.com/njoy/ENDFtk"
   url "https://github.com/njoy/ENDFtk.git", :using => :git, :tag => "v0.3.0"
   version "0.3.0"
+  revision 1
   head "https://github.com/njoy/ENDFtk.git"
 
   keg_only ""
@@ -14,8 +15,11 @@ class Endftk < Formula
     args = std_cmake_args
     args << "-DPYTHON_EXECUTABLE=#{Formula["python@3.9"].opt_bin}/python3"
 
+    patchFile = "#{File.dirname(__FILE__)}/../Patches/endftk-catch-adapter-fb84b82.patch"
+
     mkdir "Build" do
       system "cmake", *args, ".."
+      system "patch", "-p1", "-i", patchFile
       system "make", "-j#{ENV.make_jobs}", "install"
       system "make", "-j#{ENV.make_jobs}", "ENDFtk.python"
       (lib/"python3.9/site-packages").install "ENDFtk.cpython-39-darwin.so"
