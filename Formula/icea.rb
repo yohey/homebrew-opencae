fd5cd2006d49f5e2e30d3150f7cc54ebd5e9f2d1
Yohey Ishizu <yohey.ishizu@gmail.com>
2025-06-01 21:37:15 +0900

    icea: v0.2.0

---
+++ Formula/icea.rb
@@ -0,0 +1,27 @@
+class Icea < Formula
+  desc "Interface-extended CEA"
+  url "https://github.com/yohey/icea.git", :tag => "v0.2.0"
+  version "v0.2.0"
+  head "https://github.com/yohey/icea.git", :branch => "main"
+
+  depends_on "cmake" => :build
+  depends_on "gfortran" => :build
+
+  def install
+    ENV.deparallelize
+    system "cmake", "-S", ".", "-B", "Build", *std_cmake_args
+    system "cmake", "--build", "Build"
+    system "cmake", "--install", "Build"
+  end
+
+  def caveats
+    <<~EOS
+      For python to import icea you may need to set:
+        export PYTHONPATH="/opt/homebrew/opt/icea/lib"
+    EOS
+  end
+
+  test do
+    system "cmake", "--build", "Build", "--target", "test"
+  end
+end
