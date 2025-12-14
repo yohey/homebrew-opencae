922a49611d6731b5f808a579adfeee88893f8fc6
Yohey Ishizu <yohey.ishizu@gmail.com>
2025-12-14 13:05:56 +0900

    omlib-externalmedia: v4.1.1

--- Formula/omlib-externalmedia.rb
+++ Formula/omlib-externalmedia.rb
@@ -1,75 +1,39 @@
 class OmlibExternalmedia < Formula
   desc "The ExternalMedia library provides a framework for interfacing external codes computing fluid properties to Modelica.Media-compatible component models."
   homepage "https://github.com/modelica-3rdparty/ExternalMedia"
-  url "https://github.com/modelica-3rdparty/ExternalMedia.git", :using => :git, :tag => "v3.3.0"
-  version "3.3.0"
+  url "https://github.com/modelica-3rdparty/ExternalMedia.git", :using => :git, :tag => "v4.1.1"
+  version "4.1.1"
   head "https://github.com/modelica-3rdparty/ExternalMedia.git"
 
   depends_on "cmake" => :build
 
-  depends_on "yohey/opencae/openmodelica@1.18" => :recommended
-  depends_on "openmodelica" if build.without?("openmodelica@1.18")
-
-  patch :DATA
+  depends_on "yohey/opencae/openmodelica@1.25"
 
   def install
-    ENV.cxx11
-    args = std_cmake_args
+    args = std_cmake_args + %w[
+      -DCMAKE_C_COMPILER=/usr/bin/clang
+      -DCMAKE_CXX_COMPILER=/usr/bin/clang++
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+    ]
 
-    mkdir "Build" do
-      system "cmake", *args, "../Projects"
-      system "cmake", "--build", ".", "--config", "Release", "--target", "install"
-    end
+    # The current version of ExternalMedia requires you to run the configure step twice.
+    system "cmake", "-B", "build_cmake", "-S", "./Projects", *args
+    system "cmake", "-B", "build_cmake", "-S", "./Projects", *args
 
-    cd "Modelica/ExternalMedia 3.3.0/Resources/Library" do
-      system "ln", "-s", "clang130/libExternalMediaLib.dylib", "linux64/"
-      if Hardware::CPU.arm?
-        mv "linux64", "aarch64-darwin"
-      else
-        mv "linux64", "darwin64"
-      end
-    end
+    system "cmake", "--build", "build_cmake", "--config", "Release"
+    system "cmake", "--build", "build_cmake", "--config", "Release", "--target", "install"
+
+    system "mv", "Modelica/ExternalMedia", "Modelica/ExternalMedia #{version}"
 
     (lib/"omlibrary").mkpath
-    (lib/"omlibrary").install Dir["Modelica/ExternalMedia 3.3.0"]
+    (lib/"omlibrary").install Dir["Modelica/ExternalMedia #{version}"]
+
+    if Hardware::CPU.arm?
+      system "echo", "if Hardware::CPU.arm?"
+      ln_s "aarch64-darwin", lib/"omlibrary/ExternalMedia #{version}/Resources/Library/arm64-darwin"
+    end
   end
 
   # test do
   # end
 end
-
-__END__
---- a/Projects/CMakeLists.txt
-+++ b/Projects/CMakeLists.txt
-@@ -61,14 +61,15 @@ if(COOLPROP)
-     #file(ARCHIVE_EXTRACT INPUT ${CMAKE_BINARY_DIR}/coolprop.zip)
-     #execute_process(COMMAND unzip ${CMAKE_BINARY_DIR}/coolprop.zip -d "${CMAKE_BINARY_DIR}/coolprop-tmp")
-     #execute_process(COMMAND sed -i '421s;^;\#;' "${CMAKE_BINARY_DIR}/coolprop-tmp/CoolProp.sources/CMakeLists.txt")
-+    execute_process(COMMAND sed -i ".bak" "s/-stdlib=libstdc++ -mmacosx-version-min=10.6/-stdlib=libc++ -mmacosx-version-min=10.9/g" "${CMAKE_BINARY_DIR}/coolprop-tmp/CoolProp.sources/CMakeLists.txt")
-     file(RENAME "${CMAKE_BINARY_DIR}/coolprop-tmp/CoolProp.sources" "${CMAKE_CURRENT_SOURCE_DIR}/../externals/CoolProp.git")
-     file(REMOVE_RECURSE "${CMAKE_BINARY_DIR}/coolprop-tmp")
-     file(MAKE_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/CoolProp")
-   endif()
-   # Configure CMake switches for CoolProp
-   SET(COOLPROP_STATIC_LIBRARY OFF CACHE BOOL "Force the object library")
--  SET(COOLPROP_SHARED_LIBRARY OFF CACHE BOOL "Force the object library")
--  SET(COOLPROP_OBJECT_LIBRARY ON  CACHE BOOL "Force the object library")
-+  SET(COOLPROP_SHARED_LIBRARY ON  CACHE BOOL "Force the object library")
-+  SET(COOLPROP_OBJECT_LIBRARY OFF CACHE BOOL "Force the object library")
-   # Force bitness for MinGW
-   if(MINGW)
-     if(CMAKE_SIZEOF_VOID_P MATCHES "8")
-@@ -140,11 +141,11 @@ else()
- endif()
- 
- # Add the target for ExternalMedia
--add_library(${LIBRARY_NAME} STATIC ${LIB_SOURCES})
-+add_library(${LIBRARY_NAME} SHARED ${LIB_SOURCES})
- set_property(TARGET ${LIBRARY_NAME} PROPERTY VERSION ${APP_VERSION})
- target_compile_definitions(${LIBRARY_NAME} PRIVATE EXTERNALMEDIA_FLUIDPROP=$<IF:$<BOOL:${FLUIDPROP}>,1,0>)
- target_compile_definitions(${LIBRARY_NAME} PRIVATE EXTERNALMEDIA_COOLPROP=$<IF:$<BOOL:${COOLPROP}>,1,0>)
--target_compile_definitions(${LIBRARY_NAME} PRIVATE EXTERNALMEDIA_MODELICA_ERRORS=1) # Use 0 for a shared library and 1 for a static library
-+target_compile_definitions(${LIBRARY_NAME} PRIVATE EXTERNALMEDIA_MODELICA_ERRORS=0) # Use 0 for a shared library and 1 for a static library
- #target_compile_definitions(${LIBRARY_NAME} PRIVATE EXTERNALMEDIA_LIBRARY_EXPORTS) # Use this for a shared library
- if(COOLPROP)
- add_dependencies(${LIBRARY_NAME} CoolProp)
